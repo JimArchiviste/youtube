c98aca3818a2c03b0656de345f4ef3ac66be5652
JimArchiviste <jimarchiviste@Debian-GHIS>
2014-09-27 19:14:48 +0200

    l'heure du talent

--- youtube.sh
+++ youtube.sh
@@ -1,4 +1,13 @@
 test=$(cat $1)
 cd $2
 pwd
-for line in $test; do youtube-dl "$line" ; done
+for line in $test;
+do
+	trap exit INT
+	array=(${line//#?/ })
+	if [ -n "${array[1]}" ]; then
+		youtube-dl ${array[0]} -o "${array[1]}.mp4";
+	else
+		youtube-dl $array[0] -o '%(title)s';
+	fi
+done
